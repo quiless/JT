a3359fbfdbe60cd28e32e0db2a25f3cb1a11c99d
Kelvin Cleto <kcleto@outlook.com>
2018-09-20 08:16:33 -0300

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,16 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { PresentationPage } from '../pages/presentation/presentation';
+import { SignUpPage } from '../pages/sign-up/sign-up';
+import { SignInPage } from '../pages/sign-in/sign-in';
+import { VerifyNumberPage } from '../pages/verify-number/verify-number';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage:any = SignInPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
